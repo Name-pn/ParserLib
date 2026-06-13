e52ed8a54a04db80fc506dbe652a203f95d4d904
Lau-1 <lau-1038254679@yandex.ru>
2026-06-13 16:43:07 +0300

    Add ast nodes for import

--- src/parser_lib/__init__.py
+++ src/parser_lib/__init__.py
@@ -1,3 +1,4 @@
 from .LALR.LALRAnalyzerCST import LALRAnalyzerCST
 from .LALR.LALRAnalyzerAST import LALRAnalyzerAST
+from .Tree.TreeRegexAst import TreeRegexAstNode, TypeAst
 from .Grammar.Grammar import Grammar
