3ebea8b57d3dfa45d04c44e6ce89bc2ee937b1a7
Lau-1 <lau-1038254679@yandex.ru>
2026-06-19 01:05:57 +0300

    Fix OR node to concat, fix that error

--- src/parser_lib/Tree/RegexAstNodes/Concat.py
+++ src/parser_lib/Tree/RegexAstNodes/Concat.py
@@ -3,6 +3,6 @@ from parser_lib.Tree.TreeRegexAst import TreeRegexAstNode, TypeAst
 
 class ConcatNode(TreeRegexAstNode):
     def __init__(self, left, right):
-        super().__init__(TypeAst.OR)
+        super().__init__(TypeAst.CONCAT)
         self.left = left
         self.right = right
